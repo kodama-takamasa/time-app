f0dc90afdcb4dc1e89e3bc37d895a9ff66a8e2aa
kodama takamasa <kota1986.0415@gmail.com>
2025-10-29 16:48:36 +0900

    改善: スマホのアラーム設定幅を拡張

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Service Worker for Timer & Stopwatch PWA
-const CACHE_NAME = "timer-stopwatch-v1.0.9";
+const CACHE_NAME = "timer-stopwatch-v1.1.0";
 const urlsToCache = [
   "./",
   "./index.html",
